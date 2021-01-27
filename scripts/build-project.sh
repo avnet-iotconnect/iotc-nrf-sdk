7d170fefdb64d477bdbd9c14306050e9cf4e1744
Nik Markovic <nik.markovic@vensi.com>
2021-01-27 12:51:08 -0600

    github actions - ensure we build in LTE-M only (no GPS) mode

--- scripts/build-project.sh
+++ scripts/build-project.sh
@@ -8,4 +8,7 @@ source "${NCS_ROOT}"/zephyr/zephyr-env.sh
 
 "${this_dir}"/patch-gettimeofday.sh
 
-west build -p auto -b thingy91_nrf9160ns -- -DCONFIG_IOTCONNECT_CPID=\"${NRF_SAMPLE_CPID}\" -DCONFIG_IOTCONNECT_ENV=\"${NRF_SAMPLE_ENV}\"
+west build -p auto -b thingy91_nrf9160ns -- \
+  -DCONFIG_IOTCONNECT_CPID=\"${NRF_SAMPLE_CPID}\" \
+  -DCONFIG_IOTCONNECT_ENV=\"${NRF_SAMPLE_ENV}\" \
+  -DCONFIG_LTE_NETWORK_MODE_LTE_M=y
