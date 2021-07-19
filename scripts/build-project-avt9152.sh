016b9cb57aff0c19809c60069f6e07c6266358ed
Sy <jennifer.sy@avnet.com>
2021-07-19 16:53:25 +0800

    fix build-project-avt9152.sh

--- scripts/build-project-avt9152.sh
+++ scripts/build-project-avt9152.sh
@@ -8,8 +8,7 @@ source "${NCS_ROOT}"/zephyr/zephyr-env.sh
 
 "${this_dir}"/patch-gettimeofday.sh
 
-west build -p auto -b nrf9160_avt9152ns -- \
-  -d build_avt9152 \
+west build -p auto -b nrf9160_avt9152ns -d build_avt9152 -- \
   -DCONFIG_IOTCONNECT_CPID=\"${NRF_SAMPLE_CPID}\" \
   -DCONFIG_IOTCONNECT_ENV=\"${NRF_SAMPLE_ENV}\" \
   -DCONFIG_LTE_NETWORK_MODE_LTE_M=y \
