186953b28567dee6b2619135a735d2cf2eac3e79
Sy <jennifer.sy@avnet.com>
2021-07-21 15:25:23 +0800

    create copy of nrf9160_avt9152 board files inside NCS_ROOT

--- scripts/build-project-avt9152.sh
+++ scripts/build-project-avt9152.sh
@@ -11,5 +11,4 @@ source "${NCS_ROOT}"/zephyr/zephyr-env.sh
 west build -p auto -b nrf9160_avt9152ns -d build_avt9152 -- \
   -DCONFIG_IOTCONNECT_CPID=\"${NRF_SAMPLE_CPID}\" \
   -DCONFIG_IOTCONNECT_ENV=\"${NRF_SAMPLE_ENV}\" \
-  -DCONFIG_LTE_NETWORK_MODE_LTE_M=y \
-  -DBOARD_ROOT="${this_dir}"/..
+  -DCONFIG_LTE_NETWORK_MODE_LTE_M=y 
