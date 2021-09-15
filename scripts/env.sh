c72ff452f53654c92aeebcf7b43179d785e975bc
Nik Markovic <nik.markovic@vensi.com>
2021-09-15 11:44:23 -0500

    Update to 9-2019q4 arm toolchain

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,5 @@
 export CMAKE_INSTALL_URL=${CMAKE_INSTALL_URL:-https://github.com/Kitware/CMake/releases/download/v3.13.1/cmake-3.13.1-Linux-x86_64.sh}
-export GNUARMEMB_TOOLCHAIN_URL=${GNUARMEMB_TOOLCHAIN_PATH:-https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/8-2019q3/RC1.1/gcc-arm-none-eabi-8-2019-q3-update-linux.tar.bz2}
+export GNUARMEMB_TOOLCHAIN_URL=${GNUARMEMB_TOOLCHAIN_PATH:-https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2?revision=108bd959-44bd-4619-9c19-26187abf5225&hash=8B0FA405733ED93B97BAD0D2C3D3F62A}
 export DTC_INSTALL_URL=http://mirrors.kernel.org/ubuntu/pool/main/d/device-tree-compiler/device-tree-compiler_1.5.1-1_amd64.deb
 
 
