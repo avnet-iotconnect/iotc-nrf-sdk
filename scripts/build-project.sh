8410cc61973c94cc215460c4567a092c97eda812
Nik Markovic <nik.markovic@vensi.com>
2021-09-27 13:34:11 -0500

    build-project.sh: cleanup

--- scripts/build-project.sh
+++ scripts/build-project.sh
@@ -18,27 +18,17 @@ case $target in
 
 dk)
   echo "Building for DK..."
-  west build -p auto -b nrf9160dk_nrf9160ns -d build_$target -- \
-    -DCONFIG_IOTCONNECT_CPID=\"${NRF_SAMPLE_CPID}\" \
-    -DCONFIG_IOTCONNECT_ENV=\"${NRF_SAMPLE_ENV}\" \
-    -DCONFIG_LTE_NETWORK_MODE_LTE_M=y
+  target_board=nrf9160dk_nrf9160ns
   ;;
 
 thingy91)
   echo "Building for THINGY:91..."
-  west build -p auto -b thingy91_nrf9160ns -d build_$target -- \
-    -DCONFIG_IOTCONNECT_CPID=\"${NRF_SAMPLE_CPID}\" \
-    -DCONFIG_IOTCONNECT_ENV=\"${NRF_SAMPLE_ENV}\" \
-    -DCONFIG_LTE_NETWORK_MODE_LTE_M=y
+  target_board=thingy91_nrf9160ns
   ;;
 
 avt9152)
+  target_board=nrf9160_avt9152ns
   echo "Building for AVT9152-EVB..."
-  west build -p auto -b nrf9160_avt9152ns -d build_$target -- \
-    -DCONFIG_IOTCONNECT_CPID=\"${NRF_SAMPLE_CPID}\" \
-    -DCONFIG_IOTCONNECT_ENV=\"${NRF_SAMPLE_ENV}\" \
-    -DCONFIG_LTE_NETWORK_MODE_LTE_M=y \
-    -DBOARD_ROOT="${this_dir}"/..
   ;;
 
 *)
@@ -48,9 +38,19 @@ avt9152)
 
 esac
 
+build_dir=build_$target
+
+# workaround for spm config being invalid, maybe applicable only on windows?
+# echo q | west build -t spm_menuconfig -b $target_board -d $build_dir
+
+west build -p auto -b $target_board -d $build_dir -- \
+  -DCONFIG_IOTCONNECT_CPID=\"${NRF_SAMPLE_CPID}\" \
+  -DCONFIG_IOTCONNECT_ENV=\"${NRF_SAMPLE_ENV}\" \
+  -DCONFIG_LTE_NETWORK_MODE_LTE_M=y
+
 mkdir -p precompiled_image
-cp build_$target/zephyr/merged.hex precompiled_image/merged_$target.hex
-cp build_$target/zephyr/app_signed.hex precompiled_image/app_signed_$target.hex
-cp build_$target/zephyr/app_update.bin precompiled_image/app_update_$target.bin
-rm -r build_$target
+cp $build_dir/zephyr/merged.hex precompiled_image/merged_$target.hex
+cp $build_dir/zephyr/app_signed.hex precompiled_image/app_signed_$target.hex
+cp $build_dir/zephyr/app_update.bin precompiled_image/app_update_$target.bin
+rm -r $build_dir
 
